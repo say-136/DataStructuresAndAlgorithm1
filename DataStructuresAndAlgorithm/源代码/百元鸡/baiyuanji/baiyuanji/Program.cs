61a7fe19f91fdff34360a6beac4eb0861c03d094
shenaoyun-say <say@.com>
2020-01-03 11:02:19 +0800

    3

--- DataStructuresAndAlgorithm/源代码/百元鸡/baiyuanji/baiyuanji/Program.cs
+++ DataStructuresAndAlgorithm/源代码/百元鸡/baiyuanji/baiyuanji/Program.cs
@@ -8,8 +8,8 @@ namespace baiyuanji
         {
             for (int i = 3; i < 100; i += 3)
                 for (int j = 1; j < 100; j++)
-                    
-                  if(i/3+j*3+(100-i-j)*5==100 && 100-i-j>0)
+
+                    if (i / 3 + j * 3 + (100 - i - j) * 5 == 100 && 100 - i - j > 0)
                     {
                         Console.WriteLine("小鸡:" + i + "母鸡:" + j + "公鸡:" + (100 - i - j));
                     }
